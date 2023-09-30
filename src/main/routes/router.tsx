505471ce8407f05fb9b82f8b497cd5a6aec6bef2
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-30 19:45:40 -0300

    feat(main/route): add MainRouter

---
+++ src/main/routes/router.tsx
@@ -0,0 +1,10 @@
+import { BrowserRouter, Routes } from 'react-router-dom'
+
+export function MainRouter() {
+  return (
+    <BrowserRouter>
+        <Routes>
+        </Routes>
+    </BrowserRouter>
+  )
+}

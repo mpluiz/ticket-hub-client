95e1f796396f97faafc6060a506ca2294529a903
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-30 19:46:02 -0300

    feat(main): implements ApolloProvider and MainRouter

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { ApolloProvider } from '@apollo/client'
+import { client } from '@/infra/graphql/apollo'
+import { MainRouter } from '@/main/routes/router.tsx'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div>Ticket Hub</div>
+    <ApolloProvider client={client}>
+      <MainRouter/>
+    </ApolloProvider>
   </React.StrictMode>
 )

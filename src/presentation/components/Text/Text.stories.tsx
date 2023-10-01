34dd53f6c802f69cc5cc862796320c87637a3020
mpluiz <marcospaulo.luiz@gmail.com>
2023-10-01 13:46:05 -0300

    storybook(component): add Text

---
+++ src/presentation/components/Text/Text.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text } from '@/presentation/components'
+
+const meta: Meta<typeof Text> = {
+  component: Text,
+  title: 'Typography/Text'
+}
+
+export default meta
+type Story = StoryObj<typeof Text>
+
+export const Default: Story = {
+  args: { children: 'valid-text' }
+}

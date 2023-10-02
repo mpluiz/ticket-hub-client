d586f711b6ec6da2bcb3333a1a503493c055f47b
mpluiz <marcospaulo.luiz@gmail.com>
2023-10-02 15:52:24 -0300

    storybook(component): add Button

---
+++ src/presentation/components/Button/Button.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button } from '@/presentation/components'
+import { IconArrowUpRight } from '@/presentation/components/icons'
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  title: 'Form/Button'
+}
+
+export default meta
+type Story = StoryObj<typeof Button>
+
+export const Default: Story = {
+  args: { children: 'Primary' }
+}
+
+export const Icon: Story = {
+  args: { children: 'Primary', icon: <IconArrowUpRight /> }
+}

ee621b512f7bde67e67cd5dc5d04431db1fa92a4
mpluiz <marcospaulo.luiz@gmail.com>
2023-10-04 13:16:34 -0300

    feat(presentation/component): add TicketList

---
+++ src/presentation/components/TicketList/TicketList.tsx
@@ -0,0 +1,26 @@
+import { TicketListItem, TicketListItemProps } from '@/presentation/components'
+import '@/presentation/components/TicketList/TicketList.scss'
+
+interface TicketListProps {
+  tickets: TicketListItemProps[],
+}
+
+export function TicketList({ tickets }: TicketListProps) {
+  return (
+    <div className="ticket-list">
+      {tickets && tickets.map(ticket => (
+        <TicketListItem
+          key={ticket.id}
+          id={ticket.id}
+          name={ticket.name}
+          description={ticket.description}
+          imageUrl={ticket.imageUrl}
+          price={ticket.price}
+          address={ticket.address}
+          reviews={ticket.reviews}
+          onClick={ticket.onClick}
+        />
+      ))}
+    </div>
+  )
+}

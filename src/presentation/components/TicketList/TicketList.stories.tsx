76105782a16db21ad6d46dd6dffe356b87ccfbf0
mpluiz <marcospaulo.luiz@gmail.com>
2023-10-04 13:16:43 -0300

    storybook(component): add TicketList

---
+++ src/presentation/components/TicketList/TicketList.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { TicketList } from '@/presentation/components'
+
+const meta: Meta<typeof TicketList> = {
+  component: TicketList,
+  title: 'App/TicketList'
+}
+
+export default meta
+type Story = StoryObj<typeof TicketList>
+
+export const Default: Story = {
+  args: {
+    tickets: [
+      {
+        id: '1b2ccbe5a-43a8-4634-b3c6-b362986dd250',
+        name: 'Lorem ipsum dolor amet consectetur',
+        description: '',
+        imageUrl: 'https://s3-alpha-sig.figma.com/img/6100/5c68/da34365b4777e61b21faaca0291e3746?Expires=1697414400&Signature=lU7X5qVmUNss2PVDS19-uGFfP76zn7tnibMcNISdoYPozYOevu82Bx9BmDWDRX0BhzPXz8BfNfi2vhsSxVjY7ua~Fcz7VflUwWs2pOGiLUYLKehbkIlfI6aKRnmWGzhoOdy05hUYlnyDbTvT3-gF8xLol98XUcwKEbNJVmvqree-gabQnlw9JrHheBcleSRUJhmrHOw4al~bOxkmrViqLcal6pxZ8GStMDwmsQBjaco31CDuU5vbGLH~-NUiye8Qa9jEdxQrZc3M0rHwh6grhUZ9uI1JvFLlw0BjE~b1e8flLUwo89gs82Zy6qzzw8zqIPacC0IQjDS0FIl6xUyBVw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+        address: { city: 'GetYourGuide Tours & Tickets Gmbh', state: 'São Paulo', country: 'Brazil' },
+        price: { originalValue: 2351.28, discount: 960, value: 1391.28 },
+        reviews: [{ value: 7 }, { value: 3.4 }, { value: 8.5 }],
+        onClick: (id: string) => ((id))
+      },
+      {
+        id: '2b2ccbe5a-43a8-4634-b3c6-b362986dd250',
+        name: 'Lorem ipsum dolor amet consectetur',
+        description: '',
+        imageUrl: 'https://s3-alpha-sig.figma.com/img/6100/5c68/da34365b4777e61b21faaca0291e3746?Expires=1697414400&Signature=lU7X5qVmUNss2PVDS19-uGFfP76zn7tnibMcNISdoYPozYOevu82Bx9BmDWDRX0BhzPXz8BfNfi2vhsSxVjY7ua~Fcz7VflUwWs2pOGiLUYLKehbkIlfI6aKRnmWGzhoOdy05hUYlnyDbTvT3-gF8xLol98XUcwKEbNJVmvqree-gabQnlw9JrHheBcleSRUJhmrHOw4al~bOxkmrViqLcal6pxZ8GStMDwmsQBjaco31CDuU5vbGLH~-NUiye8Qa9jEdxQrZc3M0rHwh6grhUZ9uI1JvFLlw0BjE~b1e8flLUwo89gs82Zy6qzzw8zqIPacC0IQjDS0FIl6xUyBVw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+        address: { city: 'GetYourGuide Tours & Tickets Gmbh', state: 'São Paulo', country: 'Brazil' },
+        price: { originalValue: 2351.28, discount: 960, value: 1391.28 },
+        reviews: [{ value: 7 }, { value: 3.4 }, { value: 8.5 }],
+        onClick: (id: string) => ((id))
+      },
+      {
+        id: '3b2ccbe5a-43a8-4634-b3c6-b362986dd250',
+        name: 'Lorem ipsum dolor amet consectetur',
+        description: '',
+        imageUrl: 'https://s3-alpha-sig.figma.com/img/6100/5c68/da34365b4777e61b21faaca0291e3746?Expires=1697414400&Signature=lU7X5qVmUNss2PVDS19-uGFfP76zn7tnibMcNISdoYPozYOevu82Bx9BmDWDRX0BhzPXz8BfNfi2vhsSxVjY7ua~Fcz7VflUwWs2pOGiLUYLKehbkIlfI6aKRnmWGzhoOdy05hUYlnyDbTvT3-gF8xLol98XUcwKEbNJVmvqree-gabQnlw9JrHheBcleSRUJhmrHOw4al~bOxkmrViqLcal6pxZ8GStMDwmsQBjaco31CDuU5vbGLH~-NUiye8Qa9jEdxQrZc3M0rHwh6grhUZ9uI1JvFLlw0BjE~b1e8flLUwo89gs82Zy6qzzw8zqIPacC0IQjDS0FIl6xUyBVw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+        address: { city: 'GetYourGuide Tours & Tickets Gmbh', state: 'São Paulo', country: 'Brazil' },
+        price: { originalValue: 2351.28, discount: 960, value: 1391.28 },
+        reviews: [{ value: 7 }, { value: 3.4 }, { value: 8.5 }],
+        onClick: (id: string) => ((id))
+      }
+    ]
+  }
+}

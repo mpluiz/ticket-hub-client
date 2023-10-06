01b84635aec8db2ac185f0d506cd514eff3bdd0d
mpluiz <marcospaulo.luiz@gmail.com>
2023-10-06 11:30:04 -0300

    graphql(query): add GetPaginatedTickets

---
+++ src/main/graphql/queries/GetPaginatedTickets.ts
@@ -0,0 +1,36 @@
+import { gql } from '@apollo/client'
+
+export default gql`
+query GetPaginatedTickets($searchOptions: SearchOptionsInput) {
+  paginatedTickets(searchOptions: $searchOptions) {
+    pageInfo {
+      page
+      perPage
+      totalItems
+      totalPages
+    }
+
+    tickets {
+      id
+      name
+      imageUrl
+      amenities
+
+      address {
+        city
+        state
+      }
+
+      price {
+        originalValue
+        value
+        discount
+      }
+
+      reviews {
+        value
+      }
+    }
+  }
+}
+`
---
+++ src/main/graphql/queries/index.ts
@@ -0,0 +1,1 @@
+export * from '@/main/graphql/queries/GetPaginatedTickets.ts'

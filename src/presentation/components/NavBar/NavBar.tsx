0f70bf5f6fc62afe430883897fa58f9ab901e715
mpluiz <marcospaulo.luiz@gmail.com>
2023-10-06 11:33:27 -0300

    feat(presentation/component): add Cart in NavBar

--- src/presentation/components/NavBar/NavBar.tsx
+++ src/presentation/components/NavBar/NavBar.tsx
@@ -1,12 +1,17 @@
-import { ButtonCart, Logo, Text } from '@/presentation/components'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { ButtonCart, Cart, Logo, Text } from '@/presentation/components'
 import { IconQuestionCheck, IconUser } from '@/presentation/components/icons'
 import CountrySVG from '@/presentation/assets/images/country.svg'
 import '@/presentation/components/NavBar/NavBar.scss'
 
 export function NavBar() {
+  const navigate = useNavigate()
+  const [cartOpen, setCartOpen] = useState(false)
+
   return (
     <div className="navbar">
-      <Logo />
+      <Logo onClick={() => navigate('/')} />
       <div className="navbar__actions">
         <div className="navbar__quotation">
           <Text>Cotação dólar hoje: R$:5,53</Text>
@@ -17,7 +22,8 @@ export function NavBar() {
           <IconUser size={24} color="var(--brand-color-blue)" />
           <Text color="blue" weigth="bold">Entrar</Text>
         </div>
-        <ButtonCart />
+        <ButtonCart onClick={() => setCartOpen(true)} />
+        <Cart open={cartOpen} setClose={() => setCartOpen(false)} />
       </div>
     </div>
   )

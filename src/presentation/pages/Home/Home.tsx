c192526365ca61dcefa90506e66ad99ca5756112
mpluiz <marcospaulo.luiz@gmail.com>
2023-10-08 14:00:39 -0300

    feat(presentation/page): implements Pagination in HomePage

--- src/presentation/pages/Home/Home.tsx
+++ src/presentation/pages/Home/Home.tsx
@@ -1,24 +1,28 @@
-import { FormEvent, useCallback, useEffect } from 'react'
+import { FormEvent, useCallback, useEffect, useState } from 'react'
 import { useLazyQuery } from '@apollo/client'
 import { useNavigate } from 'react-router-dom'
-import { Container, DisplayLoader, Filter, NavBar, SearchBar, TicketList } from '@/presentation/components'
+import { Container, DisplayLoader, Filter, NavBar, Pagination, SearchBar, TicketList } from '@/presentation/components'
 import GetPaginatedTickets from '@/main/graphql/queries/GetPaginatedTickets.ts'
 import '@/presentation/pages/Home/Home.scss'
 
 export function HomePage() {
   const navigate = useNavigate()
+  const [currentPage, setCurrentPage] = useState(1)
+  const [searchTerm, setSearchTerm] = useState('')
   const [getPaginatedTickets, { data, loading }] = useLazyQuery(GetPaginatedTickets)
 
-  const handleGetPaginatedTickets = useCallback(async (term = '') => {
-    const searchOptions = { term, pagination: { page: 1, perPage: 6 } }
+  const handleGetPaginatedTickets = useCallback(async () => {
+    const searchOptions = { term: searchTerm, pagination: { page: currentPage, perPage: 6 } }
     await getPaginatedTickets({ variables: { searchOptions } })
-  }, [getPaginatedTickets])
+  }, [searchTerm, currentPage, getPaginatedTickets])
 
   async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
     const form = new FormData(event.currentTarget)
     const term = form.get('search') as string
-    await handleGetPaginatedTickets(term)
+    setCurrentPage(1)
+    setSearchTerm(term)
+    await handleGetPaginatedTickets()
   }
 
   async function handleNavigate(id: string) {
@@ -35,8 +39,19 @@ export function HomePage() {
       <SearchBar onSubmit={handleSubmit} />
 
       <Container className="home-page">
-        <Filter />
-        {(!loading && data) && <TicketList tickets={data.paginatedTickets.tickets} onItemClick={handleNavigate} />}
+        <div className="content">
+          <Filter />
+          {(!loading && data) && <TicketList tickets={data.paginatedTickets.tickets} onItemClick={handleNavigate} />}
+        </div>
+
+        {data && (
+          <Pagination
+            numberOfPages={data.paginatedTickets.pageInfo.totalPages}
+            numberOfItems={data.paginatedTickets.pageInfo.totalItems}
+            page={currentPage}
+            onChangePage={(page) => setCurrentPage(page)}
+          />
+        )}
       </Container>
 
       {loading && <DisplayLoader/>}

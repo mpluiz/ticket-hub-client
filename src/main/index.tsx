1af7ea514d500ac2063629491ec576d71d41ebb3
mpluiz <marcospaulo.luiz@gmail.com>
2023-10-09 00:46:41 -0300

    feat(main): implements sentry

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import * as Sentry from '@sentry/react'
+import SentryConfig from '@/infra/monitoring/sentry.ts'
 
 import { ApolloProvider } from '@apollo/client'
 import { client } from '@/infra/graphql/apollo'
@@ -8,6 +10,8 @@ import { MainRouter } from '@/main/routes/router.tsx'
 import '@/presentation/assets/styles/normalize.css'
 import '@/presentation/assets/styles/global.css'
 import '@/presentation/assets/styles/variables.css'
+
+Sentry.init(SentryConfig)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
